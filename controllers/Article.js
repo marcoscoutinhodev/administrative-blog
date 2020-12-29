a38b58fad9ad86fb63d3878fcba6cfc254ecdbf4
Marcos Vinicius Coutinho <marcoscoutinhodev@outlook.com>
2020-12-29 20:13:46 -0300

    Article form and saving data to the database

--- controllers/Article.js
+++ controllers/Article.js
@@ -1,8 +1,37 @@
 const express = require("express");
 const router = express.Router();
+const Category = require("../models/Category");
+const Article = require("../models/Article");
+const slugify = require("slugify");
 
-router.get("/articles", (req, res) => {
-    res.send("Article Page")
+router.get("/admin/articles", (req, res) => {
+    res.render("admin/article/index")
+})
+
+router.get("/admin/article/new", (req, res) => {
+    Category
+        .findAll()
+        .then((categories) => {
+            res.render("admin/article/new", { categories });
+        })
+});
+
+router.post("/admin/article/save", (req, res) => {
+    const { title, body, category } = req.body;
+
+    Article
+        .create({
+            title,
+            slug: slugify(title),
+            body,
+            categoryId: category
+        })
+        .then(() => {
+            res.redirect("/admin/articles");
+        })
+        .catch((err) => {
+            console.log(`An unexpected error has occurred: ${err}`);
+        });
 });
 
 module.exports = router;
--- database/database.js
+++ database/database.js
@@ -3,7 +3,8 @@ require("dotenv").config();
 
 const connection = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_TYPE
+    dialect: process.env.DB_TYPE,
+    timezone: process.env.DB_TIMEZONE
 });
 
 module.exports = connection;
